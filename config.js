34064ff5d21442e51f1df20c083bc03bc3cded47
agenciaorigen <agenciaorigenigr@gmail.com>
2026-08-21 22:02:43 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // Completá estos dos valores con los de tu proyecto:
 // Supabase > Project Settings > API
 // ============================================================
-const SUPABASE_URL = "PEGA_ACA_TU_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "PEGA_ACA_TU_SUPABASE_ANON_KEY";
+const SUPABASE_URL = "https://utrgavidbaczabxkirwo.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "sb_secret_UDpZRd5P-0tvYNOepFL92w_EZTO4GpO";
