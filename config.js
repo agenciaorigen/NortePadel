873d845139554cc409179cd38e977a55307e1cb0
agenciaorigen <agenciaorigenigr@gmail.com>
2026-08-21 22:03:51 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -4,4 +4,4 @@
 // Supabase > Project Settings > API
 // ============================================================
 const SUPABASE_URL = "https://utrgavidbaczabxkirwo.supabase.co/rest/v1/";
-const SUPABASE_ANON_KEY = "sb_secret_UDpZRd5P-0tvYNOepFL92w_EZTO4GpO";
+const SUPABASE_ANON_KEY = "sb_publishable_rpPhxqyIlj7mX8kjOZRgWg_DFHRLTDx";
